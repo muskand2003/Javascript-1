80d94931226e4223847c1e77776427af7455ff41
muskand2003 <148890938+muskand2003@users.noreply.github.com>
2024-03-16 02:11:46 +0000

    conversion of datatypes is done

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,35 @@
+ let score = "undefined"
+
+
+//  console.log(typeof score)
+//  console.log(score)
+
+  let scoreIsNumber = Number(score);
+ console.log(typeof scoreIsNumber);
+ console.log(scoreIsNumber)
+
+ // Input=>TypeafterConversion=>Output
+ // 33 => Number => 33
+ // 33abc=> Number => NaN
+ // Null => Number => NaN
+ //undefined => Number =>NaN
+
+ let IsLoggedin = "Muskan";
+
+ let booleanIsLoggedin = Boolean(IsLoggedin)
+//  console.log(typeof booleanIsLoggedin)
+//  console.log(booleanIsLoggedin)
+
+ // 1=> Boolean =>true; 0=> Boolean=> true;
+ // ""=>Boolean =>false
+ // "value"=>Boolean => true
+
+ let someNumber = 33;
+
+ let StringSomeNumber = String(someNumber);
+//  console.log(StringSomeNumber);
+//  console.log(typeof StringSomeNumber);
+
+
+
+
