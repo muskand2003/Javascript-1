c35d3ed7ce4ac5b1a170bbd6d4c7441dbc75a46d
muskand2003 <148890938+muskand2003@users.noreply.github.com>
2024-03-18 15:50:19 +0000

    data-summary done

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,32 @@
+
+// concept of bigInt
+// const bigNumber = 8898932893820382309283902n;
+// console.log(typeof bigNumber)
+
+
+// concept of symbol 
+
+// const id = Symbol("123")
+// const AnothersameId =Symbol("123")
+// console.log(id)
+// console.log(AnothersameId)
+
+// console.log(id == AnothersameId)
+
+
+const KpopIdols = ["Jungkook","V","lisa"]
+console.log(typeof KpopIdols)
+
+const obj = {
+    name : "muskan",
+    age : 20
+}
+console.log(typeof obj)
+
+
+
+const myfunction = function(){
+    console.log("Hello World")
+}
+
+console.log(typeof myfunction)
