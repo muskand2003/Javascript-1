495d81cd1c2ec514605ac4732ec44e952d51b698
muskand2003 <148890938+muskand2003@users.noreply.github.com>
2024-03-19 16:47:30 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,12 @@
+const name = "Hitesh"
+
+const repocount = 10;
+
+console.log(`My name is ${name} and my repocount is ${repocount}`)
+
+// Another way to declare string---->
+
+const gameName = new String('Muskanss') 
+
+console.log(gameName[1]);
+console.log(gameName.__proto__)
