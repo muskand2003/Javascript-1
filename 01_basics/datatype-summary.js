62ef57bbfb44c0db420593960c0ee7d1e97fa8d5
muskand2003 <148890938+muskand2003@users.noreply.github.com>
2024-03-19 01:50:35 +0000

    Data-summary done

--- 01_basics/datatype-summary.js
+++ 01_basics/datatype-summary.js
@@ -15,18 +15,48 @@
 
 
 const KpopIdols = ["Jungkook","V","lisa"]
-console.log(typeof KpopIdols)
+// console.log(typeof KpopIdols)
 
 const obj = {
     name : "muskan",
     age : 20
 }
-console.log(typeof obj)
+// console.log(typeof obj)
 
 
 
 const myfunction = function(){
-    console.log("Hello World")
+    // console.log("Hello World")
 }
 
-console.log(typeof myfunction)
+// console.log(typeof myfunction)
+
+
+//-----------------------------------------------//
+
+// Stack Memory ---> used by primitive datatypes which means they are given the value duplicate forms.
+
+let MyYoutubechannelName = "MuskanStudying"
+
+let MyNewYoutubeChannelName = MyYoutubechannelName;
+
+MyNewYoutubeChannelName = "StudywithMe"
+
+
+console.log(MyYoutubechannelName);
+console.log(MyNewYoutubeChannelName);
+
+// Heap Memory ---> used by Non-primitive type which uses the refernce that means the changes are directly made to the original value.
+
+
+let InstagramAccount = {
+    username : "Muskan",
+    password : 12345
+}
+
+let NewInstagramAccount = InstagramAccount;
+
+NewInstagramAccount.password = 234567;
+
+console.log(InstagramAccount.password);
+console.log(NewInstagramAccount.password);
