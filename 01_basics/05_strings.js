89da51569d31060152099f4b88c4fd11c5c3c97c
muskand2003 <148890938+muskand2003@users.noreply.github.com>
2024-03-23 16:14:34 +0000

    strings are  going

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -8,5 +8,17 @@ console.log(`My name is ${name} and my repocount is ${repocount}`)
 
 const gameName = new String('Muskanss') 
 
-console.log(gameName[1]);
-console.log(gameName.__proto__)
+// console.log(gameName[1]);
+// console.log(gameName.__proto__)
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('a'));
+
+// If you give negative value in substring it will just ignore it.
+const AnotherString = gameName.substring(0,4)
+console.log(AnotherString);
+
+const Newstring = gameName.slice(-8,7);
+console.log(Newstring);
