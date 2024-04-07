e79fc15ca84f17ca78379403ba16eaff84aa9d6d
muskand2003 <148890938+muskand2003@users.noreply.github.com>
2024-04-07 01:43:48 +0000

    Learned About Matha and Num

--- 01_basics/06_Num-and-Maths.js
+++ 01_basics/06_Num-and-Maths.js
@@ -1,10 +1,40 @@
 const score = 400;
 // console.log(typeof score)
-console.log(score)
+// console.log(score)
 
 const balance = new Number(100);
-console.log(balance);
+// console.log(balance);
 
-console.log(score.toString)
+// console.log(score.toString)
 
-console.log(balance.toFixed(2))
+// console.log(balance.toFixed(2)) //Used for adding decimal point at last.
+
+const otherNumber = 126.988873
+// console.log(otherNumber.toPrecision(3))
+
+
+const Num = 100000000
+// console.log(Num.toLocaleString('en-IN'))
+
+//++++++++++++++++Math++++++++++++++++++++++//
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(3.678))
+// console.log(Math.ceil(2.3));
+// console.log(Math.floor(3.9));
+// console.log(Math.min(3,5,1,0,9))
+// console.log(Math.max(11,21,18,38,10))
+
+
+// ************Math.random()********
+
+
+console.log(Math.random());
+console.log((Math.random()*10 )+ 1);
+
+
+const min = 1;
+const max = 6;
+
+console.log(Math.floor(Math.random() * max - min + 1 )+ min );
